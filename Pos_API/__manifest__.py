12a9a7778464fd3e0f940168b55f9407dc37f7e4
@dh2305 <dudhat.hk@gmail.com>
2023-01-18 22:23:26 +0530

    [FIX] Fix minor version and update dependencies

--- Pos_API/__manifest__.py
+++ Pos_API/__manifest__.py
@@ -17,10 +17,10 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.1.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','point_of_sale'],
+    'depends': ['base','point_of_sale', 'account', 'crm'],
 
     'data': [
         # 'security/ir.model.access.csv',
