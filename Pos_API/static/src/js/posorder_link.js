63d5f386141809171d1405e784558342e19485f0
unknown <bosamia.karan@gmail.com>
2023-01-18 22:46:04 +0530

    [IMP] Code clean up for some files

--- Pos_API/static/src/js/posorder_link.js
+++ Pos_API/static/src/js/posorder_link.js
@@ -1,18 +1,3 @@
-// odoo.define('Pos_API.orderlink', function(require){
-//     'use strict';
-//     var models = require('point_of_sale.models');
-//     var _super_product = models.PosModel.prototype;
-//     models.PosModel = models.PosModel.extend({
-//         initialize: function(session, attributes){
-//             var self = this;
-//             console.log('\n\n this one is added\n\n');
-//             // models.load_fields('product.product', ['new_field']);
-//             // _super_product.initialize.apply(this, arguments);
-//         }
-//     });
-// });
-
-
 odoo.define('Pos_API.orderlink', function (require) {
     'use strict';
 
@@ -35,28 +20,19 @@ const Raju = (Order) => class Raju extends Order {
         return Object.keys(this.pos.validated_orders_name_server_id_map)[0];
     }
 
-        //@override
+    //@override
     export_as_JSON() {
         const json = super.export_as_JSON(...arguments);
-        // if (this.pos.config.module_pos_restaurant) {
-            // if (this.pos.config.iface_floorplan) {
-            //     json.table_id = this.tableId
-            // }
-        // json.links = this.links;
-        // }
-        // if (this.pos.config.iface_printers) {
-        //     json.multiprint_resume = JSON.stringify(this.printedResume);
-        //     // so that it can be stored in local storage and be used when loading the pos in the floorscreen
-        //     json.printing_changes = JSON.stringify(this.printingChanges);
-        // }
         return json;
     }
+
     //@override
     init_from_JSON(json) {
         super.init_from_JSON(...arguments);
         // this.links = json.links;
 
     }
+    
     //@override
     export_for_printing() {
         const json = super.export_for_printing(...arguments);
@@ -64,8 +40,7 @@ const Raju = (Order) => class Raju extends Order {
     }
    
 }
+
 Registries.Model.extend(Order, Raju);
  
-    // const models = require('point_of_sale.models');
-    // models.load_fields('res.partner', 'links');
 });
